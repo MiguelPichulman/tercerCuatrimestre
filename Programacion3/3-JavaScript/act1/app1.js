1e29f5b43a8363a3f8aad5e3387b6697256727a3
Miguel Pichulman <miguel.a.pichulman@gmail.com>
2026-04-20 18:23:11 -0300

    update

---
+++ Programacion3/3-JavaScript/act1/app1.js
@@ -0,0 +1,19 @@
+const nombre= "Alumno";
+let edad = 15;
+let aprobado=true;
+let curso;
+let proyecto= null;
+
+const pi= 3.1416;
+let numero=0;
+
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("Aprobado:", aprobado);
+console.log("Curso:", curso);
+console.log("Proyecto:", proyecto);
+
+console.log("El valor de PI es:", pi);
+
+numero=5;
+console.log("Nuevo valor de numero:", numero);
---
+++ Programacion3/3-JavaScript/act1/app3.js
@@ -0,0 +1,18 @@
+let a = 10;
+let b= 3;
+
+console.log("suma:",a+b);
+console.log("resta:",a-b);
+console.log("multiplicacion:",a*b);
+console.log("division:",a/b);
+console.log("resto:", a%b);
+
+let esMayor= true;
+let tieneDNI= false;
+console.log(esMayor && tieneDNI);
+console.log(esMayor || tieneDNI);
+console.log(!esMayor);
+
+let edad= 18;
+let mensaje= (edad>=18) ? "puede votar" : "es menor de edad";
+console.log(mensaje);
