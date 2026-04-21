e51fca1d8345e443c1ea2e6454d7dafa64f81e8e
Miguel Pichulman <miguel.a.pichulman@gmail.com>
2026-04-21 16:45:35 -0300

    update JS

--- Programacion3/3-JavaScript/Practica/js/data.js
+++ Programacion3/3-JavaScript/Practica/js/data.js
@@ -5,14 +5,32 @@ const productos = [
         id: 1,
         nombre: "Hamburguesa Triple",
         descripcion: "Triple carne, cheddar y bacon",
-        precio: 25000, imagen: "hamburguesa",
+        precio: 25000,
+        imagen:"./assets/burger.png",
         categoria: "Hamburguesas"
      },
      { 
         id: 2,
         nombre: "Pizza Muzzarella",
         descripcion: "Salsa casera y orégano",
-        precio: 18000, imagen: "Pizza",
+        precio: 18000,
+        imagen:"./assets/pizza.png",
         categoria: "Pizzas"
-    }
-     ];
+    },
+    {
+      id:3,
+      nombre:"Papas Fritas",
+      descripcion:"Papas Fritas con Cheddar",
+      precio:10000,
+      imagen:"./assets/papas.png",
+      categoria:"Papas Fritas"
+    },
+    {
+      id:4,
+      nombre:"Gaseosa",
+      descripcion:"Coca-Cola 500ml",
+      precio:5000,
+      imagen:"./assets/gaseosa.png",
+      categoria:"Bebidas"
+    } 
+   ];
