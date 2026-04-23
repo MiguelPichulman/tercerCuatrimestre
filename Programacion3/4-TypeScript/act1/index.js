8c7f18fe2141725ccf3e92dc36ace0368ed095c0
Miguel Pichulman <miguel.a.pichulman@gmail.com>
2026-04-23 10:29:23 -0300

    update

---
+++ Programacion3/4-TypeScript/act1/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function sumarJS(a, b) {
+    return a + b;
+}
+console.log(sumarJS(5, "10"));
+console.log(sumarJS(5, 10));
---
+++ Programacion3/4-TypeScript/act1/index.ts
@@ -0,0 +1,5 @@
+function sumarJS (a:any, b:any){
+    return a+b;
+}
+console.log(sumarJS(5,"10"));
+console.log(sumarJS(5,10));
